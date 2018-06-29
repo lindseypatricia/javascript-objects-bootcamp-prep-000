9beaac7fa121e6a57d82ea3e96380cf8ba005a21
Lindsey Brown <brown.lindsey.p@gmail.com>
2018-06-29 21:10:41 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -7,4 +7,4 @@ function updatePlaylist(playlist, artist, song) {
   return Object.assign({}, playlist, { [artist]: song })
 }
 
-updatePlaylist(playlist, 'Phil Ochs', )
+updatePlaylist(playlist, 'Phil Ochs', "Here's to the State of Mississippi")
