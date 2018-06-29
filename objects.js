786c6d229e44989bc4544cc68415004901cddeb6
Lindsey Brown <brown.lindsey.p@gmail.com>
2018-06-29 21:06:51 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,3 +1,12 @@
 var playlist = {
-  
+  'My Bloody Valentine': 'Sometimes',
+  'Slowdive': 'Alison',
+}
+
+function updatePlaylist(playlist, artist, song) {
+  return Object.assign({}, playlist, { [artist]: song })
+}
+
+function removeFromPlaylist(playlist, artist) {
+  return Object.assign({}, playlist, delete.artist)
 }
