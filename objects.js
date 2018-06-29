c1c115414dc96bdf92674285ae839685ccf1d74a
Lindsey Brown <brown.lindsey.p@gmail.com>
2018-06-29 21:17:17 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -11,7 +11,7 @@ function updatePlaylist(playlist, artist, song) {
 updatePlaylist(playlist, 'Phil Ochs', "Here's to the State of Mississippi")
 
 function removeFromPlaylist(playlist, artist) {
-  delete playlist.(artist);
+  delete playlist.[artist];
   return playlist
 }
 
